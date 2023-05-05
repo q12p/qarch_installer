949203fc03ebed20ff17494268dd1ae4f9b1c849
q12p <q.pw.q12@gmail.com>
2023-05-05 22:02:37 +0200

    Fixed script

--- files/.config/sxhkd/mute.sh
+++ files/.config/sxhkd/mute.sh
@@ -3,7 +3,7 @@
 # Check the current mute status of the first sink
 current_device=$(pacmd list-sinks | grep "*" | awk '{print $NF}')
 
-state=$(pacmd list-sinks |  grep -A 11 "index: 4" | grep "muted" | awk '{print $NF}')
+state=$(pacmd list-sinks |  grep -A 11 "index: $current_device" | grep "muted" | awk '{print $NF}')
 
 pactl set-sink-mute $current_device 0
 # If the mute status is "yes", unmute the volume
